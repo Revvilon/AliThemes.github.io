167e762409a66f741aac9e21d83ff59c9b880418
Revvilon <55360334+Revvilon@users.noreply.github.com>
2020-06-06 12:16:37 +0200

    dwadwadwa
    s

--- index.js
+++ index.js
@@ -30,3 +30,24 @@ function myFaq() {
     }
 }
 
+let onyx = document.getElementById('changemyhref');
+let secretVariable = 0;
+
+function myFunction(x) {
+    if (secretVariable > 4) {
+        secretVariable = 0;
+        onyx.href="https://discordapp.com/users/173723461619810304";
+        onyx.target="_blank"
+    }
+    secretVariable = secretVariable + x;
+    console.log(secretVariable);
+    if (secretVariable == 5) {
+        console.log("you did it");
+        onyx.href="subpages/secretpage.html";
+        onyx.target="_self"
+    }
+}
+
+function resetVariable() {
+    secretVariable = 0;
+}
